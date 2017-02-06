ad9b7bb0611f27fb4e11072576a2ed48d9428db7
Matthew <Matthew@DESKTOP-F814V9B>
2017-02-06 10:24:59 -0500

    Removed deprecated method

--- src/main/java/com/serotonin/bacnet4j/util/DiscoveryUtils.java
+++ src/main/java/com/serotonin/bacnet4j/util/DiscoveryUtils.java
@@ -23,116 +23,119 @@
  * without being obliged to provide the source code for any proprietary components.
  *
  * See www.infiniteautomation.com for commercial license options.
- * 
+ *
  * @author Matthew Lohbihler
  */
 package com.serotonin.bacnet4j.util;
 
-import java.util.Map;
-
 import com.serotonin.bacnet4j.LocalDevice;
 import com.serotonin.bacnet4j.RemoteDevice;
-import com.serotonin.bacnet4j.event.DeviceEventAdapter;
 import com.serotonin.bacnet4j.exception.BACnetException;
 import com.serotonin.bacnet4j.service.acknowledgement.ReadPropertyAck;
 import com.serotonin.bacnet4j.service.confirmed.ReadPropertyRequest;
-import com.serotonin.bacnet4j.service.unconfirmed.WhoIsRequest;
-import com.serotonin.bacnet4j.transport.Transport;
 import com.serotonin.bacnet4j.type.Encodable;
-import com.serotonin.bacnet4j.type.constructed.ServicesSupported;
 import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
-import com.serotonin.bacnet4j.type.primitive.CharacterString;
 import com.serotonin.bacnet4j.type.primitive.ObjectIdentifier;
-import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
 
 public class DiscoveryUtils {
-    public static RemoteDevice discoverDevice(LocalDevice localDevice, final int deviceId) {
-        // Send a WhoIs to the device. Use a listener to notify of a response so that we can return as soon as possible.
-        final Object lock = new Object();
-        DeviceEventAdapter listener = new DeviceEventAdapter() {
-            @Override
-            public void iAmReceived(RemoteDevice d) {
-                if (d.getInstanceNumber() == deviceId) {
-                    synchronized (lock) {
-                        lock.notify();
-                    }
-                }
-            }
-        };
+    //    public static RemoteDevice discoverDevice(final LocalDevice localDevice, final int deviceId) {
+    //        // Send a WhoIs to the device. Use a listener to notify of a response so that we can return as soon as possible.
+    //        final Object lock = new Object();
+    //        final DeviceEventAdapter listener = new DeviceEventAdapter() {
+    //            @Override
+    //            public void iAmReceived(final RemoteDevice d) {
+    //                if (d.getInstanceNumber() == deviceId) {
+    //                    synchronized (lock) {
+    //                        lock.notify();
+    //                    }
+    //                }
+    //            }
+    //        };
+    //
+    //        try {
+    //            localDevice.getEventHandler().addListener(listener);
+    //
+    //            synchronized (lock) {
+    //                // Send a WhoIs for the device.
+    //                final UnsignedInteger id = new UnsignedInteger(deviceId);
+    //                localDevice.sendGlobalBroadcast(new WhoIsRequest(id, id));
+    //
+    //                try {
+    //                    lock.wait(Transport.DEFAULT_TIMEOUT);
+    //                } catch (final InterruptedException e) {
+    //                    // Ignore
+    //                }
+    //            }
+    //        } finally {
+    //            localDevice.getEventHandler().removeListener(listener);
+    //        }
+    //
+    //        // Get the device from the local.
+    //        final RemoteDevice d = localDevice.getRemoteDevice(deviceId);
+    //        if (d == null)
+    //            return null;
+    //
+    //        try {
+    //            final Map<PropertyIdentifier, Encodable> map = RequestUtils.getProperties(localDevice, d, null,
+    //                    PropertyIdentifier.protocolServicesSupported);
+    //            final Encodable e = map.get(PropertyIdentifier.protocolServicesSupported);
+    //            if (e instanceof ServicesSupported)
+    //                d.setDeviceProperty(PropertyIdentifier.protocolServicesSupported, e);
+    //            //TODO could be BACnetError here or other, should log this?
+    //        } catch (final BACnetException e) {
+    //            // ignore
+    //        }
+    //
+    //        return d;
+    //    }
 
-        try {
-            localDevice.getEventHandler().addListener(listener);
-
-            synchronized (lock) {
-                // Send a WhoIs for the device.
-                UnsignedInteger id = new UnsignedInteger(deviceId);
-                localDevice.sendGlobalBroadcast(new WhoIsRequest(id, id));
-
-                try {
-                    lock.wait(Transport.DEFAULT_TIMEOUT);
-                }
-                catch (InterruptedException e) {
-                    // Ignore
-                }
-            }
-        }
-        finally {
-            localDevice.getEventHandler().removeListener(listener);
-        }
-
-        // Get the device from the local.
-        RemoteDevice d;
-        try {
-            d = localDevice.getRemoteDevice(deviceId);
-        }
-        catch (BACnetException e) {
-            return null;
-        }
-
-        try {
-            Map<PropertyIdentifier, Encodable> map = RequestUtils.getProperties(localDevice, d, null,
-                    PropertyIdentifier.protocolServicesSupported);
-            Encodable e =  map.get(PropertyIdentifier.protocolServicesSupported);
-            if(e instanceof ServicesSupported)
-            	d.setServicesSupported((ServicesSupported)e);
-            //TODO could be BACnetError here or other, should log this?
-        }
-        catch (BACnetException e) {
-            // ignore
-        }
-
-        return d;
-    }
-
-    public static void getExtendedDeviceInformation(LocalDevice localDevice, RemoteDevice d) throws BACnetException {
-        ObjectIdentifier oid = d.getObjectIdentifier();
+    public static void getExtendedDeviceInformation(final LocalDevice localDevice, final RemoteDevice d)
+            throws BACnetException {
+        final ObjectIdentifier oid = d.getObjectIdentifier();
 
         // Get the device's supported services
         if (d.getServicesSupported() == null) {
-            ReadPropertyAck supportedServicesAck = (ReadPropertyAck) localDevice.send(d,
-                    new ReadPropertyRequest(oid, PropertyIdentifier.protocolServicesSupported)).get();
-            d.setServicesSupported((ServicesSupported) supportedServicesAck.getValue());
+            final ReadPropertyAck supportedServicesAck = (ReadPropertyAck) localDevice
+                    .send(d, new ReadPropertyRequest(oid, PropertyIdentifier.protocolServicesSupported)).get();
+            d.setDeviceProperty(PropertyIdentifier.protocolServicesSupported, supportedServicesAck.getValue());
         }
 
         // Uses the readProperties method here because this list will probably be extended.
-        PropertyReferences properties = new PropertyReferences();
-        properties.add(oid, PropertyIdentifier.objectName);
-        properties.add(oid, PropertyIdentifier.protocolVersion);
-        properties.add(oid, PropertyIdentifier.vendorIdentifier);
-        properties.add(oid, PropertyIdentifier.modelName);
+        final PropertyReferences properties = new PropertyReferences();
+        addIfMissing(d, properties, PropertyIdentifier.objectName);
+        addIfMissing(d, properties, PropertyIdentifier.protocolVersion);
+        addIfMissing(d, properties, PropertyIdentifier.vendorIdentifier);
+        addIfMissing(d, properties, PropertyIdentifier.modelName);
 
-        PropertyValues values = RequestUtils.readProperties(localDevice, d, properties, null);
+        if (properties.size() > 0) {
+            // Only send a request if we have to.
+            final PropertyValues values = RequestUtils.readProperties(localDevice, d, properties, null);
 
-        d.setName(values.getString(oid, PropertyIdentifier.objectName));
-        d.setProtocolVersion((UnsignedInteger) values.getNullOnError(oid, PropertyIdentifier.protocolVersion));
+            values.forEach((opr) -> {
+                final Encodable value = values.getNullOnError(oid, opr.getPropertyIdentifier());
+                d.setDeviceProperty(opr.getPropertyIdentifier(), value);
+            });
 
-        UnsignedInteger vendorIdentifier = (UnsignedInteger) values.getNullOnError(oid,
-                PropertyIdentifier.vendorIdentifier);
-        if (vendorIdentifier != null)
-            d.setVendorId(vendorIdentifier.intValue());
+            //            d.setDeviceProperty(PropertyIdentifier.objectName,
+            //                    values.getNullOnError(oid, PropertyIdentifier.objectName));
+            //            d.setDeviceProperty(PropertyIdentifier.protocolVersion,
+            //                    values.getNullOnError(oid, PropertyIdentifier.protocolVersion));
+            //
+            //            final UnsignedInteger vendorIdentifier = (UnsignedInteger) values.getNullOnError(oid,
+            //                    PropertyIdentifier.vendorIdentifier);
+            //            if (vendorIdentifier != null)
+            //                d.setDeviceProperty(PropertyIdentifier.vendorIdentifier, vendorIdentifier);
+            //
+            //            final CharacterString modelName = (CharacterString) values.getNullOnError(oid,
+            //                    PropertyIdentifier.modelName);
+            //            if (modelName != null)
+            //                d.setDeviceProperty(PropertyIdentifier.modelName, modelName);
+        }
+    }
 
-        CharacterString modelName = (CharacterString) values.getNullOnError(oid, PropertyIdentifier.modelName);
-        if (modelName != null)
-            d.setModelName(modelName.getValue());
+    private static void addIfMissing(final RemoteDevice d, final PropertyReferences properties,
+            final PropertyIdentifier pid) {
+        if (d.getDeviceProperty(pid) == null)
+            properties.add(d.getObjectIdentifier(), pid);
     }
 }
