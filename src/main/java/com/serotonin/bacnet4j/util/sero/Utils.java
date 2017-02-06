a279112d5adbfab46bff1cf89b27632d52a40082
Matthew <Matthew@DESKTOP-F814V9B>
2017-02-06 10:18:13 -0500

    Replaced equals method with Java8 version

--- src/main/java/com/serotonin/bacnet4j/util/sero/Utils.java
+++ src/main/java/com/serotonin/bacnet4j/util/sero/Utils.java
@@ -23,26 +23,15 @@
  * without being obliged to provide the source code for any proprietary components.
  *
  * See www.infiniteautomation.com for commercial license options.
- * 
+ *
  * @author Matthew Lohbihler
  */
 package com.serotonin.bacnet4j.util.sero;
 
 public class Utils {
-    // TODO replace with Objects.equals when upgraded to Java8.
-    public static boolean equals(final Object object1, final Object object2) {
-        if (object1 == object2) {
-            return true;
-        }
-        if (object1 == null || object2 == null) {
-            return false;
-        }
-        return object1.equals(object2);
-    }
-
-    public static byte[] commaSeparatedHex(String s) {
-        String[] parts = s.split(",");
-        byte[] result = new byte[parts.length];
+    public static byte[] commaSeparatedHex(final String s) {
+        final String[] parts = s.split(",");
+        final byte[] result = new byte[parts.length];
         for (int i = 0; i < parts.length; i++)
             result[i] = (byte) Integer.parseInt(parts[i], 16);
         return result;
