3194e38ccb5a3331962d48a4b6894feade506cbb
Matthew <Matthew@DESKTOP-F814V9B>
2017-03-28 21:54:11 -0400

    Enhanced fault algo to handle multiple data types.

--- src/main/java/com/serotonin/bacnet4j/obj/mixin/event/faultAlgo/FaultOutOfRangeAlgo.java
+++ src/main/java/com/serotonin/bacnet4j/obj/mixin/event/faultAlgo/FaultOutOfRangeAlgo.java
@@ -9,7 +9,10 @@ import com.serotonin.bacnet4j.type.constructed.FaultParameter.AbstractFaultParam
 import com.serotonin.bacnet4j.type.constructed.FaultParameter.FaultOutOfRange;
 import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
 import com.serotonin.bacnet4j.type.enumerated.Reliability;
+import com.serotonin.bacnet4j.type.primitive.Double;
 import com.serotonin.bacnet4j.type.primitive.Real;
+import com.serotonin.bacnet4j.type.primitive.SignedInteger;
+import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
 
 //13.4.7
 public class FaultOutOfRangeAlgo extends FaultAlgorithm {
@@ -36,7 +39,7 @@ public class FaultOutOfRangeAlgo extends FaultAlgorithm {
         return evaluate( //
                 bo.get(minimumNormalValueProperty), //
                 bo.get(maximumNormalValueProperty), //
-                (Real) newMonitoredValue, //
+                newMonitoredValue, //
                 bo.get(currentReliabilityProperty));
     }
 
@@ -47,18 +50,65 @@ public class FaultOutOfRangeAlgo extends FaultAlgorithm {
         return evaluate( //
                 p.getMinNormalValue().getValue(), //
                 p.getMaxNormalValue().getValue(), //
-                (Real) newMonitoredValue, //
+                newMonitoredValue, //
                 currentReliability);
     }
 
-    private static Reliability evaluate(final Real minimumNormalValue, final Real maximumNormalValue,
-            final Real monitoredValue, Reliability currentReliability) {
+    private static Reliability evaluate(final Encodable minimumNormalValue, final Encodable maximumNormalValue,
+            final Encodable monitoredValue, final Reliability currentReliability) {
+        // All of the Encodable parameters must have the same type, either Real, Unsigned, Double, or Signed.
+        if (monitoredValue instanceof Real) {
+            return evaluateFloating(((Real) minimumNormalValue).floatValue(), ((Real) maximumNormalValue).floatValue(),
+                    ((Real) monitoredValue).floatValue(), currentReliability);
+        }
+        if (monitoredValue instanceof Double) {
+            return evaluateFloating(((Double) minimumNormalValue).doubleValue(),
+                    ((Double) maximumNormalValue).doubleValue(), ((Double) monitoredValue).doubleValue(),
+                    currentReliability);
+        }
+        if (monitoredValue instanceof UnsignedInteger) {
+            return evaluateInteger(((UnsignedInteger) minimumNormalValue).longValue(),
+                    ((UnsignedInteger) maximumNormalValue).longValue(), ((UnsignedInteger) monitoredValue).longValue(),
+                    currentReliability);
+        }
+        if (monitoredValue instanceof SignedInteger) {
+            return evaluateInteger(((SignedInteger) minimumNormalValue).longValue(),
+                    ((SignedInteger) maximumNormalValue).longValue(), ((SignedInteger) monitoredValue).longValue(),
+                    currentReliability);
+        }
+        throw new RuntimeException("Unsupported type: " + monitoredValue.getClass());
+    }
+
+    private static Reliability evaluateInteger(final long min, final long max, final long value,
+            Reliability currentReliability) {
         if (currentReliability == null)
             currentReliability = Reliability.noFaultDetected;
 
-        final float min = minimumNormalValue.floatValue();
-        final float max = maximumNormalValue.floatValue();
-        final float value = monitoredValue.floatValue();
+        Reliability newReliability = null;
+
+        if (Reliability.noFaultDetected.equals(currentReliability) && value < min) // (a)
+            newReliability = Reliability.underRange;
+        else if (Reliability.noFaultDetected.equals(currentReliability) && value > max) // (b)
+            newReliability = Reliability.overRange;
+        else if (Reliability.underRange.equals(currentReliability) && value > max) // (c)
+            newReliability = Reliability.overRange;
+        else if (Reliability.overRange.equals(currentReliability) && value < min) // (d)
+            newReliability = Reliability.underRange;
+        else if (Reliability.underRange.equals(currentReliability) && value >= min) // (e)
+            newReliability = Reliability.noFaultDetected;
+        else if (Reliability.overRange.equals(currentReliability) && value <= max) // (f)
+            newReliability = Reliability.noFaultDetected;
+
+        if (newReliability != null)
+            LOG.debug("FaultState evaluated new reliability: {}", newReliability);
+
+        return newReliability;
+    }
+
+    private static Reliability evaluateFloating(final double min, final double max, final double value,
+            Reliability currentReliability) {
+        if (currentReliability == null)
+            currentReliability = Reliability.noFaultDetected;
 
         Reliability newReliability = null;
 
