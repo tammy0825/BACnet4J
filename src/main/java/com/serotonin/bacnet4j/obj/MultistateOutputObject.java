a144b24edaa53be2c5e98bee9b15fdfc589a2fb6
Matthew <Matthew@DESKTOP-F814V9B>
2017-03-30 15:52:21 -0400

    MultistateOutput object

---
+++ src/main/java/com/serotonin/bacnet4j/obj/MultistateOutputObject.java
@@ -0,0 +1,98 @@
+package com.serotonin.bacnet4j.obj;
+
+import java.util.Objects;
+
+import com.serotonin.bacnet4j.LocalDevice;
+import com.serotonin.bacnet4j.exception.BACnetServiceException;
+import com.serotonin.bacnet4j.obj.mixin.CommandableMixin;
+import com.serotonin.bacnet4j.obj.mixin.HasStatusFlagsMixin;
+import com.serotonin.bacnet4j.obj.mixin.MultistateMixin;
+import com.serotonin.bacnet4j.obj.mixin.ReadOnlyPropertyMixin;
+import com.serotonin.bacnet4j.obj.mixin.WritablePropertyOutOfServiceMixin;
+import com.serotonin.bacnet4j.obj.mixin.event.IntrinsicReportingMixin;
+import com.serotonin.bacnet4j.obj.mixin.event.eventAlgo.CommandFailureAlgo;
+import com.serotonin.bacnet4j.type.constructed.BACnetArray;
+import com.serotonin.bacnet4j.type.constructed.DeviceObjectReference;
+import com.serotonin.bacnet4j.type.constructed.EventTransitionBits;
+import com.serotonin.bacnet4j.type.constructed.OptionalUnsigned;
+import com.serotonin.bacnet4j.type.constructed.StatusFlags;
+import com.serotonin.bacnet4j.type.constructed.ValueSource;
+import com.serotonin.bacnet4j.type.enumerated.EventState;
+import com.serotonin.bacnet4j.type.enumerated.NotifyType;
+import com.serotonin.bacnet4j.type.enumerated.ObjectType;
+import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
+import com.serotonin.bacnet4j.type.enumerated.Reliability;
+import com.serotonin.bacnet4j.type.primitive.Boolean;
+import com.serotonin.bacnet4j.type.primitive.CharacterString;
+import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
+
+public class MultistateOutputObject extends BACnetObject {
+    public MultistateOutputObject(final LocalDevice localDevice, final int instanceNumber, final String name,
+            final int numberOfStates, final BACnetArray<CharacterString> stateText, final int presentValueBase1,
+            final int relinquishDefaultBase1, final boolean outOfService) throws BACnetServiceException {
+        super(localDevice, ObjectType.multiStateOutput, instanceNumber, name);
+
+        if (numberOfStates < 1) {
+            throw new IllegalArgumentException("numberOfStates cannot be less than 1");
+        }
+
+        final ValueSource valueSource = new ValueSource(new DeviceObjectReference(localDevice.getId(), getId()));
+
+        writePropertyInternal(PropertyIdentifier.eventState, EventState.normal);
+        writeProperty(valueSource, PropertyIdentifier.presentValue, new UnsignedInteger(presentValueBase1));
+        writePropertyInternal(PropertyIdentifier.outOfService, new Boolean(true));
+        writePropertyInternal(PropertyIdentifier.statusFlags, new StatusFlags(false, false, false, true));
+        writePropertyInternal(PropertyIdentifier.reliability, Reliability.noFaultDetected);
+        writePropertyInternal(PropertyIdentifier.interfaceValue, new OptionalUnsigned());
+
+        // Mixins
+        addMixin(new HasStatusFlagsMixin(this));
+        addMixin(new CommandableMixin(this, PropertyIdentifier.presentValue));
+        addMixin(new MultistateMixin(this));
+
+        writePropertyInternal(PropertyIdentifier.numberOfStates, new UnsignedInteger(numberOfStates));
+        if (stateText != null) {
+            if (numberOfStates != stateText.getCount()) {
+                throw new IllegalArgumentException("numberOfStates does not match state text count");
+            }
+            writeProperty(null, PropertyIdentifier.stateText, stateText);
+        }
+        writeProperty(valueSource, PropertyIdentifier.presentValue, new UnsignedInteger(presentValueBase1));
+        if (!outOfService) {
+            writePropertyInternal(PropertyIdentifier.outOfService, new Boolean(outOfService));
+        }
+
+        addMixin(new WritablePropertyOutOfServiceMixin(this, PropertyIdentifier.presentValue,
+                PropertyIdentifier.reliability));
+        addMixin(new ReadOnlyPropertyMixin(this, PropertyIdentifier.ackedTransitions,
+                PropertyIdentifier.eventTimeStamps, PropertyIdentifier.eventMessageTexts));
+
+        _supportCommandable(new UnsignedInteger(relinquishDefaultBase1));
+        _supportValueSource();
+    }
+
+    public MultistateOutputObject supportCovReporting() {
+        _supportCovReporting(null);
+        return this;
+    }
+
+    public MultistateOutputObject supportIntrinsicReporting(final int timeDelay, final int notificationClass,
+            final int feedbackValue, final EventTransitionBits eventEnable, final NotifyType notifyType,
+            final int timeDelayNormal) {
+        Objects.requireNonNull(eventEnable);
+        Objects.requireNonNull(notifyType);
+
+        writePropertyInternal(PropertyIdentifier.timeDelay, new UnsignedInteger(timeDelay));
+        writePropertyInternal(PropertyIdentifier.notificationClass, new UnsignedInteger(notificationClass));
+        writePropertyInternal(PropertyIdentifier.feedbackValue, new UnsignedInteger(feedbackValue));
+        writePropertyInternal(PropertyIdentifier.eventEnable, eventEnable);
+        writePropertyInternal(PropertyIdentifier.notifyType, notifyType);
+        writePropertyInternal(PropertyIdentifier.timeDelayNormal, new UnsignedInteger(timeDelayNormal));
+        writePropertyInternal(PropertyIdentifier.eventDetectionEnable, new Boolean(true));
+
+        addMixin(new IntrinsicReportingMixin(this, new CommandFailureAlgo(), null, PropertyIdentifier.presentValue,
+                new PropertyIdentifier[] { PropertyIdentifier.presentValue, PropertyIdentifier.feedbackValue }));
+
+        return this;
+    }
+}
---
+++ src/test/java/com/serotonin/bacnet4j/obj/MultistateOutputObjectTest.java
@@ -0,0 +1,326 @@
+package com.serotonin.bacnet4j.obj;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.serotonin.bacnet4j.LocalDevice;
+import com.serotonin.bacnet4j.RemoteDevice;
+import com.serotonin.bacnet4j.exception.BACnetServiceException;
+import com.serotonin.bacnet4j.npdu.test.TestNetwork;
+import com.serotonin.bacnet4j.transport.DefaultTransport;
+import com.serotonin.bacnet4j.type.AmbiguousValue;
+import com.serotonin.bacnet4j.type.constructed.BACnetArray;
+import com.serotonin.bacnet4j.type.constructed.Destination;
+import com.serotonin.bacnet4j.type.constructed.DeviceObjectPropertyReference;
+import com.serotonin.bacnet4j.type.constructed.EventTransitionBits;
+import com.serotonin.bacnet4j.type.constructed.PropertyValue;
+import com.serotonin.bacnet4j.type.constructed.Recipient;
+import com.serotonin.bacnet4j.type.constructed.SequenceOf;
+import com.serotonin.bacnet4j.type.constructed.StatusFlags;
+import com.serotonin.bacnet4j.type.constructed.TimeStamp;
+import com.serotonin.bacnet4j.type.enumerated.ErrorClass;
+import com.serotonin.bacnet4j.type.enumerated.ErrorCode;
+import com.serotonin.bacnet4j.type.enumerated.EventState;
+import com.serotonin.bacnet4j.type.enumerated.EventType;
+import com.serotonin.bacnet4j.type.enumerated.NotifyType;
+import com.serotonin.bacnet4j.type.enumerated.PropertyIdentifier;
+import com.serotonin.bacnet4j.type.eventParameter.CommandFailure;
+import com.serotonin.bacnet4j.type.eventParameter.EventParameter;
+import com.serotonin.bacnet4j.type.notificationParameters.CommandFailureNotif;
+import com.serotonin.bacnet4j.type.notificationParameters.NotificationParameters;
+import com.serotonin.bacnet4j.type.primitive.Boolean;
+import com.serotonin.bacnet4j.type.primitive.CharacterString;
+import com.serotonin.bacnet4j.type.primitive.UnsignedInteger;
+
+import lohbihler.warp.WarpClock;
+
+public class MultistateOutputObjectTest {
+    static final Logger LOG = LoggerFactory.getLogger(MultistateOutputObjectTest.class);
+
+    private final WarpClock clock = new WarpClock();
+    private LocalDevice d1;
+    private LocalDevice d2;
+    private RemoteDevice rd1;
+    private RemoteDevice rd2;
+    private MultistateOutputObject mo;
+    private NotificationClassObject nc;
+
+    @Before
+    public void before() throws Exception {
+        d1 = new LocalDevice(1, new DefaultTransport(new TestNetwork(1, 0))).withClock(clock).initialize();
+        d2 = new LocalDevice(2, new DefaultTransport(new TestNetwork(2, 0))).withClock(clock).initialize();
+
+        rd1 = d2.getRemoteDevice(1).get();
+        rd2 = d1.getRemoteDevice(2).get();
+
+        mo = new MultistateOutputObject(d1, 0, "mo", 5,
+                new BACnetArray<>(new CharacterString("Off"), //
+                        new CharacterString("On"), //
+                        new CharacterString("Auto"), //
+                        new CharacterString("Fan"), //
+                        new CharacterString("Other")),
+                2, 5, false);
+        nc = new NotificationClassObject(d1, 17, "nc17", 100, 5, 200, new EventTransitionBits(false, false, false));
+    }
+
+    @After
+    public void after() {
+        d1.terminate();
+        d2.terminate();
+    }
+
+    @Test
+    public void initialization() throws Exception {
+        new MultistateOutputObject(d1, 1, "mo1", 7, null, 1, 1, false);
+
+        try {
+            new MultistateOutputObject(d1, 2, "mv2", 0, null, 1, 1, false);
+            Assert.fail("Should have thrown an IllegalArgumentException");
+        } catch (@SuppressWarnings("unused") final IllegalArgumentException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void inconsistentStateText() throws Exception {
+        try {
+            new MultistateOutputObject(d1, 1, "mv1", 7, new BACnetArray<>(new CharacterString("a")), 1, 1, true);
+            Assert.fail("Should have thrown an IllegalArgumentException");
+        } catch (@SuppressWarnings("unused") final IllegalArgumentException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void missingStateText() throws Exception {
+        final MultistateOutputObject mv = new MultistateOutputObject(d1, 1, "mv1", 7, null, 1, 1, true);
+
+        try {
+            mv.writeProperty(null,
+                    new PropertyValue(PropertyIdentifier.stateText, new BACnetArray<>(new CharacterString("a"))));
+            fail("Should have thrown an exception");
+        } catch (final BACnetServiceException e) {
+            assertEquals(ErrorClass.property, e.getErrorClass());
+            assertEquals(ErrorCode.inconsistentConfiguration, e.getErrorCode());
+        }
+    }
+
+    @Test
+    public void stateText() throws Exception {
+        final MultistateOutputObject mv = new MultistateOutputObject(d1, 1, "mv1", 7, null, 1, 1, true);
+
+        mv.writeProperty(null,
+                new PropertyValue(PropertyIdentifier.stateText,
+                        new BACnetArray<>(new CharacterString("a"), new CharacterString("b"), new CharacterString("c"),
+                                new CharacterString("d"), new CharacterString("e"), new CharacterString("f"),
+                                new CharacterString("g"))));
+
+        mv.writeProperty(null, new PropertyValue(PropertyIdentifier.numberOfStates, new UnsignedInteger(6)));
+        assertEquals(
+                new BACnetArray<>(new CharacterString("a"), new CharacterString("b"), new CharacterString("c"),
+                        new CharacterString("d"), new CharacterString("e"), new CharacterString("f")),
+                mv.get(PropertyIdentifier.stateText));
+
+        mv.writeProperty(null, new PropertyValue(PropertyIdentifier.numberOfStates, new UnsignedInteger(8)));
+        assertEquals(new BACnetArray<>(new CharacterString("a"), new CharacterString("b"), new CharacterString("c"),
+                new CharacterString("d"), new CharacterString("e"), new CharacterString("f"), new CharacterString(""),
+                new CharacterString("")), mv.get(PropertyIdentifier.stateText));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void intrinsicReporting() throws Exception {
+        final SequenceOf<Destination> recipients = nc.get(PropertyIdentifier.recipientList);
+        recipients.add(new Destination(new Recipient(rd2.getAddress()), new UnsignedInteger(10), new Boolean(true),
+                new EventTransitionBits(true, true, true)));
+
+        // Create an event listener on d2 to catch the event notifications.
+        final EventNotifListener listener = new EventNotifListener();
+        d2.getEventHandler().addListener(listener);
+
+        mo.supportIntrinsicReporting(5, 17, 2, new EventTransitionBits(true, true, true), NotifyType.alarm, 12);
+        // Ensure that initializing the intrinsic reporting didn't fire any notifications.
+        Thread.sleep(40);
+        assertEquals(0, listener.notifs.size());
+
+        // Check the starting values.
+        assertEquals(new UnsignedInteger(2), mo.get(PropertyIdentifier.presentValue));
+        assertEquals(new UnsignedInteger(2), mo.get(PropertyIdentifier.feedbackValue));
+
+        // Do a state change. Write a value to indicate a command failure. After 5s the alarm will be raised.
+        mo.writePropertyInternal(PropertyIdentifier.feedbackValue, new UnsignedInteger(1));
+        clock.plus(4500, TimeUnit.MILLISECONDS, 4500, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.normal, mo.getProperty(PropertyIdentifier.eventState)); // Still normal at this point.
+        clock.plus(600, TimeUnit.MILLISECONDS, 600, TimeUnit.MILLISECONDS, 0, 80);
+        assertEquals(EventState.offnormal, mo.getProperty(PropertyIdentifier.eventState));
+        assertEquals(new StatusFlags(true, false, false, false), mo.getProperty(PropertyIdentifier.statusFlags));
+
+        // Ensure that a proper looking event notification was received.
+        assertEquals(1, listener.notifs.size());
+        Map<String, Object> notif = listener.notifs.remove(0);
+        assertEquals(new UnsignedInteger(10), notif.get("processIdentifier"));
+        assertEquals(rd1.getObjectIdentifier(), notif.get("initiatingDevice"));
+        assertEquals(mo.getId(), notif.get("eventObjectIdentifier"));
+        assertEquals(((BACnetArray<TimeStamp>) mo.getProperty(PropertyIdentifier.eventTimeStamps))
+                .getBase1(EventState.offnormal.getTransitionIndex()), notif.get("timeStamp"));
+        assertEquals(new UnsignedInteger(17), notif.get("notificationClass"));
+        assertEquals(new UnsignedInteger(100), notif.get("priority"));
+        assertEquals(EventType.commandFailure, notif.get("eventType"));
+        assertEquals(null, notif.get("messageText"));
+        assertEquals(NotifyType.alarm, notif.get("notifyType"));
+        assertEquals(new Boolean(false), notif.get("ackRequired"));
+        assertEquals(EventState.normal, notif.get("fromState"));
+        assertEquals(EventState.offnormal, notif.get("toState"));
+        CommandFailureNotif commandFailure = ((NotificationParameters) notif.get("eventValues")).getParameter();
+        assertEquals(new UnsignedInteger(2),
+                AmbiguousValue.convertTo(commandFailure.getCommandValue(), UnsignedInteger.class));
+        assertEquals(new StatusFlags(true, false, false, false), commandFailure.getStatusFlags());
+        assertEquals(new UnsignedInteger(1),
+                AmbiguousValue.convertTo(commandFailure.getFeedbackValue(), UnsignedInteger.class));
+
+        // Return to normal. After 12s the notification will be sent.
+        mo.writePropertyInternal(PropertyIdentifier.presentValue, new UnsignedInteger(1));
+        clock.plus(11500, TimeUnit.MILLISECONDS, 11500, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.offnormal, mo.getProperty(PropertyIdentifier.eventState)); // Still offnormal at this point.
+        clock.plus(600, TimeUnit.MILLISECONDS, 600, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.normal, mo.getProperty(PropertyIdentifier.eventState));
+        assertEquals(new StatusFlags(false, false, false, false), mo.getProperty(PropertyIdentifier.statusFlags));
+
+        // Ensure that a proper looking event notification was received.
+        assertEquals(1, listener.notifs.size());
+        notif = listener.notifs.remove(0);
+        assertEquals(new UnsignedInteger(10), notif.get("processIdentifier"));
+        assertEquals(rd1.getObjectIdentifier(), notif.get("initiatingDevice"));
+        assertEquals(mo.getId(), notif.get("eventObjectIdentifier"));
+        assertEquals(((BACnetArray<TimeStamp>) mo.getProperty(PropertyIdentifier.eventTimeStamps))
+                .getBase1(EventState.normal.getTransitionIndex()), notif.get("timeStamp"));
+        assertEquals(new UnsignedInteger(17), notif.get("notificationClass"));
+        assertEquals(new UnsignedInteger(200), notif.get("priority"));
+        assertEquals(EventType.commandFailure, notif.get("eventType"));
+        assertEquals(null, notif.get("messageText"));
+        assertEquals(NotifyType.alarm, notif.get("notifyType"));
+        assertEquals(new Boolean(false), notif.get("ackRequired"));
+        assertEquals(EventState.offnormal, notif.get("fromState"));
+        assertEquals(EventState.normal, notif.get("toState"));
+        commandFailure = ((NotificationParameters) notif.get("eventValues")).getParameter();
+        assertEquals(new UnsignedInteger(1),
+                AmbiguousValue.convertTo(commandFailure.getCommandValue(), UnsignedInteger.class));
+        assertEquals(new StatusFlags(false, false, false, false), commandFailure.getStatusFlags());
+        assertEquals(new UnsignedInteger(1),
+                AmbiguousValue.convertTo(commandFailure.getFeedbackValue(), UnsignedInteger.class));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void algorithmicReporting() throws Exception {
+        // Set the feedback value to match the prsent value
+        mo.writePropertyInternal(PropertyIdentifier.feedbackValue, new UnsignedInteger(2));
+
+        // Check the starting values.
+        assertEquals(new UnsignedInteger(2), mo.get(PropertyIdentifier.presentValue));
+        assertEquals(new UnsignedInteger(2), mo.get(PropertyIdentifier.feedbackValue));
+
+        final DeviceObjectPropertyReference ref = new DeviceObjectPropertyReference(1, mo.getId(),
+                PropertyIdentifier.presentValue);
+        final EventEnrollmentObject ee = new EventEnrollmentObject(d1, 0, "ee", ref, NotifyType.alarm,
+                new EventParameter(new CommandFailure(new UnsignedInteger(30),
+                        new DeviceObjectPropertyReference(1, mo.getId(), PropertyIdentifier.feedbackValue))),
+                new EventTransitionBits(true, true, true), 17, 1000, null, null);
+
+        // Set up the notification destination
+        final SequenceOf<Destination> recipients = nc.get(PropertyIdentifier.recipientList);
+        recipients.add(new Destination(new Recipient(rd2.getAddress()), new UnsignedInteger(10), new Boolean(true),
+                new EventTransitionBits(true, true, true)));
+
+        // Create an event listener on d2 to catch the event notifications.
+        final EventNotifListener listener = new EventNotifListener();
+        d2.getEventHandler().addListener(listener);
+
+        // Ensure that initializing the event enrollment object didn't fire any notifications.
+        Thread.sleep(40);
+        assertEquals(EventState.normal, ee.getProperty(PropertyIdentifier.eventState));
+        assertEquals(0, listener.notifs.size());
+
+        //
+        // Go to off normal.
+        mo.writePropertyInternal(PropertyIdentifier.feedbackValue, new UnsignedInteger(1));
+        // Allow the EE to poll
+        clock.plus(1100, TimeUnit.MILLISECONDS, 1100, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.normal, ee.getProperty(PropertyIdentifier.eventState));
+        // Wait until just before the time delay.
+        clock.plus(29500, TimeUnit.MILLISECONDS, 29500, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.normal, ee.getProperty(PropertyIdentifier.eventState));
+        // Wait until after the time delay.
+        clock.plus(600, TimeUnit.MILLISECONDS, 600, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.offnormal, ee.getProperty(PropertyIdentifier.eventState));
+
+        // Ensure that a proper looking event notification was received.
+        assertEquals(1, listener.notifs.size());
+        Map<String, Object> notif = listener.notifs.remove(0);
+        assertEquals(new UnsignedInteger(10), notif.get("processIdentifier"));
+        assertEquals(d1.getId(), notif.get("initiatingDevice"));
+        assertEquals(ee.getId(), notif.get("eventObjectIdentifier"));
+        assertEquals(((BACnetArray<TimeStamp>) ee.getProperty(PropertyIdentifier.eventTimeStamps))
+                .getBase1(EventState.offnormal.getTransitionIndex()), notif.get("timeStamp"));
+        assertEquals(new UnsignedInteger(17), notif.get("notificationClass"));
+        assertEquals(new UnsignedInteger(100), notif.get("priority"));
+        assertEquals(EventType.commandFailure, notif.get("eventType"));
+        assertEquals(null, notif.get("messageText"));
+        assertEquals(NotifyType.alarm, notif.get("notifyType"));
+        assertEquals(new Boolean(false), notif.get("ackRequired"));
+        assertEquals(EventState.normal, notif.get("fromState"));
+        assertEquals(EventState.offnormal, notif.get("toState"));
+        CommandFailureNotif commandFailure = ((NotificationParameters) notif.get("eventValues")).getParameter();
+        assertEquals(new UnsignedInteger(2),
+                AmbiguousValue.convertTo(commandFailure.getCommandValue(), UnsignedInteger.class));
+        assertEquals(new StatusFlags(false, false, false, false), commandFailure.getStatusFlags());
+        assertEquals(new UnsignedInteger(1),
+                AmbiguousValue.convertTo(commandFailure.getFeedbackValue(), UnsignedInteger.class));
+
+        //
+        // Return to normal
+        mo.writePropertyInternal(PropertyIdentifier.presentValue, new UnsignedInteger(1));
+        // Allow the EE to poll
+        clock.plus(1100, TimeUnit.MILLISECONDS, 1100, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.offnormal, ee.getProperty(PropertyIdentifier.eventState));
+        // Wait until just before the time delay.
+        clock.plus(29500, TimeUnit.MILLISECONDS, 29500, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.offnormal, ee.getProperty(PropertyIdentifier.eventState));
+        // Wait until after the time delay.
+        clock.plus(600, TimeUnit.MILLISECONDS, 600, TimeUnit.MILLISECONDS, 0, 40);
+        assertEquals(EventState.normal, ee.getProperty(PropertyIdentifier.eventState));
+
+        // Ensure that a proper looking event notification was received.
+        assertEquals(1, listener.notifs.size());
+        notif = listener.notifs.remove(0);
+        assertEquals(new UnsignedInteger(10), notif.get("processIdentifier"));
+        assertEquals(d1.getId(), notif.get("initiatingDevice"));
+        assertEquals(ee.getId(), notif.get("eventObjectIdentifier"));
+        assertEquals(((BACnetArray<TimeStamp>) ee.getProperty(PropertyIdentifier.eventTimeStamps))
+                .getBase1(EventState.normal.getTransitionIndex()), notif.get("timeStamp"));
+        assertEquals(new UnsignedInteger(17), notif.get("notificationClass"));
+        assertEquals(new UnsignedInteger(200), notif.get("priority"));
+        assertEquals(EventType.commandFailure, notif.get("eventType"));
+        assertEquals(null, notif.get("messageText"));
+        assertEquals(NotifyType.alarm, notif.get("notifyType"));
+        assertEquals(new Boolean(false), notif.get("ackRequired"));
+        assertEquals(EventState.offnormal, notif.get("fromState"));
+        assertEquals(EventState.normal, notif.get("toState"));
+        commandFailure = ((NotificationParameters) notif.get("eventValues")).getParameter();
+        assertEquals(new UnsignedInteger(1),
+                AmbiguousValue.convertTo(commandFailure.getCommandValue(), UnsignedInteger.class));
+        assertEquals(new StatusFlags(false, false, false, false), commandFailure.getStatusFlags());
+        assertEquals(new UnsignedInteger(1),
+                AmbiguousValue.convertTo(commandFailure.getFeedbackValue(), UnsignedInteger.class));
+    }
+}
